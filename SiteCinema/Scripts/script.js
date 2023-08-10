27345b529c243452500ca8a8389d4e571def744e
Riquelmy Henrique Silva <83371189+riquelmyhsilva@users.noreply.github.com>
2023-08-10 08:45:57 -0300

    update: fix erros 100% working

--- SiteCinema/Scripts/script.js
+++ SiteCinema/Scripts/script.js
@@ -1,102 +1,104 @@
 // JavaScript Document
+
 const buttonBarbie = document.getElementById('button-barbie');
 
-buttonBarbie.addEventListener('click', function() {
-  window.location.href = 'Home/movieBarbie';
+buttonBarbie.addEventListener('click', function () {
+    window.location.href = '/Home/movieBarbie';
 });
 
 const buttonBezouroAzul = document.getElementById('button-bezouro-azul');
 
-buttonBezouroAzul.addEventListener('click', function() {
-  window.location.href = 'Home/movieBezouroAzul';
+buttonBezouroAzul.addEventListener('click', function () {
+    window.location.href = '/Home/movieBezouroAzul';
 });
 
 const buttonMissaoImpossivel = document.getElementById('button-missao-impossivel');
 
-buttonMissaoImpossivel.addEventListener('click', function() {
-    window.location.href = 'Home/filmeIndisponivel';
+buttonMissaoImpossivel.addEventListener('click', function () {
+    window.location.href = '/Home/filmeIndisponivel';
 });
 
 const buttonHomemAranhaAav = document.getElementById('button-homem-aranha-aav');
 
 buttonHomemAranhaAav.addEventListener('click', function () {
-    window.location.href = 'Home/filmeIndisponivel';
+    window.location.href = '/Home/filmeIndisponivel';
+});
 
-    const buttonElementos = document.getElementById('button-elementos');
+const buttonElementos = document.getElementById('button-elementos');
 
-    buttonElementos.addEventListener('click', function () {
-        window.location.href = 'Home/filmeIndisponivel';
-    });
+buttonElementos.addEventListener('click', function () {
+    window.location.href = '/Home/filmeIndisponivel';
+});
 
-    const buttonDuna = document.getElementById('button-duna');
+const buttonDuna = document.getElementById('button-duna');
 
-    buttonDuna.addEventListener('click', function () {
-        window.location.href = 'Home/filmeIndisponivel';
-    });
+buttonDuna.addEventListener('click', function () {
+    window.location.href = '/Home/filmeIndisponivel';
+});
 
-    const buttonOppenheimer = document.getElementById('button-oppenheimer');
+const buttonOppenheimer = document.getElementById('button-oppenheimer');
 
-    buttonOppenheimer.addEventListener('click', function () {
-        window.location.href = 'Home/filmeIndisponivel';
-    });
+buttonOppenheimer.addEventListener('click', function () {
+    window.location.href = '/Home/filmeIndisponivel';
+});
 
-    const buttonSobrenatural = document.getElementById('button-sobrenatural');
+const buttonSobrenatural = document.getElementById('button-sobrenatural');
 
-    buttonSobrenatural.addEventListener('click', function () {
-        window.location.href = 'Home/filmeIndisponivel';
-    });
+buttonSobrenatural.addEventListener('click', function () {
+    window.location.href = '/Home/filmeIndisponivel';
+});
 
-    const buttonTartarugasNCM = document.getElementById('button-tartarugas-ncm');
+const buttonTartarugasNCM = document.getElementById('button-tartarugas-ncm');
 
-    buttonTartarugasNCM.addEventListener('click', function () {
-        window.location.href = 'Home/filmeIndisponivel';
-    });
+buttonTartarugasNCM.addEventListener('click', function () {
+    window.location.href = '/Home/filmeIndisponivel';
+});
 
-    const buttonIconDolby = document.getElementById('icon-dolby');
+const buttonIconDolby = document.getElementById('icon-dolby');
 
-    buttonIconDolby.addEventListener('click', function () {
-        window.location.href = 'Home/filmeIndisponivel';
-    });
+buttonIconDolby.addEventListener('click', function () {
+    window.location.href = '/Home/Index';
+});
 
-    const buttonMenu = document.getElementById('button-menu');
+const buttonMenu = document.getElementById('button-menu');
 
-    buttonMenu.addEventListener('click', function () {
-        window.location.href = 'Home/filmeIndisponivel';
-    });
+buttonMenu.addEventListener('click', function () {
+    window.location.href = '/Home/Index';
+});
 
-    const slider = document.querySelector('.slider');
-    const slides = document.querySelectorAll('.slide');
-    const leftArrow = document.querySelector('.left-arrow');
-    const rightArrow = document.querySelector('.right-arrow');
-    let currentSlide = 0;
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+let currentSlide = 0;
 
-    function showSlide(index) {
-        slides.forEach((slide, i) => {
-            if (i === index) {
-                slide.style.display = 'block';
-            } else {
-                slide.style.display = 'none';
-            }
-        });
-    }
-
-    function nextSlide() {
-        currentSlide++;
-        if (currentSlide >= slides.length) {
-            currentSlide = 0;
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
         }
-        showSlide(currentSlide);
+    });
+}
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
     }
-
-    function prevSlide() {
-        currentSlide--;
-        if (currentSlide < 0) {
-            currentSlide = slides.length - 1;
-        }
-        showSlide(currentSlide);
-    }
-
-    leftArrow.addEventListener('click', prevSlide);
-    rightArrow.addEventListener('click', nextSlide);
-
     showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide--;
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    }
+    showSlide(currentSlide);
+}
+
+leftArrow.addEventListener('click', prevSlide);
+rightArrow.addEventListener('click', nextSlide);
+
+showSlide(currentSlide);
